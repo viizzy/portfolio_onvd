cac1605731db07ad96aaf0e80dce00a4f04ea230
Viktor <viktor00allegri@gmail.com>
2022-04-29 03:11:09 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 gsap.from('.nav', {opacity: 0, duration: 1, x: -50, ease: 'slow(0.7, 0.7, false)'});
-// gsap.from('.class-x', {opacity: 0, duration: 1, y: 50, ease: 'Power2.easeInOut'});
+// gsap.from('.title', {opacity: 0, duration: 1, y: 50, ease: 'Power2.easeInOut'});
 
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.from(".class-x", {
-//     y: -100,
-//     duration: 2,
-//     scrollTrigger: ".class-x"
-// });
+gsap.from(".education .col-lg-6", {
+    y: -200,
+    duration: 1,
+    scrollTrigger: ".education .col-lg-6"
+});
+
+gsap.from(".lang", {
+    y: -200,
+    duration: 1,
+    scrollTrigger: ".lang"
+});
 
 gsap.from(".ht", {
     y: 300,
@@ -23,10 +29,26 @@ gsap.from(".hx", {
     scrollTrigger: ".hx"
 });
 
+gsap.from(".education-title", {
+    y: -300,
 
-// *** vanilla tilt *** //
+    duration: 1,
+    scrollTrigger: ".education-title"
+});
+
+gsap.from(".skills", {
+    x: -300,
+
+    duration: 1,
+    scrollTrigger: ".skills"
+});
+
 VanillaTilt.init(document.querySelector(".header-rs"), {
     max: 15,
     speed: 400
-})
+});
 
+VanillaTilt.init(document.querySelector(".skills-ls"), {
+    max: 15,
+    speed: 400
+})
